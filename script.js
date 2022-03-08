27a047724af39498c9899ad4d58ee143997df69d
Angelica Arrubla <ange.arrubla@gmail.com>
2022-03-08 17:30:41 -0500

    ajustes del modal

--- script.js
+++ script.js
@@ -1,17 +1,29 @@
-const modal= document.querySelector('.modal')
-const button = document.querySelector('#button');
-const close = document.querySelector('.modal__content--close')
+const modal = document.querySelector('.modal');
+const buttonMommy = document.querySelector('#button-mommy');
+const buttonPhantom = document.querySelector('#button-phantom');
+const buttonCat = document.querySelector('#button-cat');
+const closeButton = document.querySelector('.modal__content--close');
 
-for(let i = 0; i < button.length; i++)
-{
-    button[i].addEventListener('click', () => {
-        modal.classList.remove('hidden');
-        modal.classList.add('visible');
-    })
-}
+buttonMommy.addEventListener('click', () => {
+    modal.classList.remove('hidden');
+    modal.classList.add('visible');
+});
+buttonCat.addEventListener('click', () => {
+    modal.classList.remove('hidden');
+    modal.classList.add('visible');
+});
+buttonPhantom.addEventListener('click', () => {
+    // 1ra Forma
+    // modal.className = 'modal visible';
+    //2da Forma
+    // modal.style.display = 'grid';
 
-close.addEventListener('click', () => {
-    console.log('here')
+    modal.classList.remove('hidden');
+    modal.classList.add('visible');
+});
+closeButton.addEventListener('click', () => {
+    // modal.style.display = 'none';
+
     modal.classList.remove('visible');
     modal.classList.add('hidden');
-})
+});
